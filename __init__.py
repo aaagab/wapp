6155c28fb53c56fdbb1709052b6df47911aa1e0f
gabaaa <not-set>
2022-12-13 10:54:14 -0600

    first commit after clone

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: Web App Builder
+# licenses: MIT 
+__version__ = "1.0.0"
+
+# from .dev.wapp import wapp
+# from .gpkgs import message as msg
+# from .gpkgs.nargs import Nargs
+# from .gpkgs.etconf import Etconf

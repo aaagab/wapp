5e53cfcf478fd1b32425fa097a060a5d0cc88f4a
gabaaa <gabaaa.dev@gmail.com>
2023-10-20 15:28:09 -0500

    added --msal command

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@
 # authors: Gabriel Auger
 # name: Web App Builder
 # licenses: MIT 
-__version__= "3.0.0"
+__version__= "3.1.0"
 
 from .dev.backend import backend_build, backend_start, backend_publish, backend_deploy, backend_dotnet
 from .dev.frontend import frontend_build, frontend_start, frontend_npm
 from .dev.substitute import substitute
+from .dev.msal import msal_signin
 from .dev.helpers import get_direpa_publish
 from .gpkgs import message as msg
 from .gpkgs.nargs import Nargs
